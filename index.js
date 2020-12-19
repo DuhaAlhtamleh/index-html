d91e0853f8e45c65b37b64320be31c382d29bde0
DuhaAlhtamleh <eng.duddy1997@gmail.com>
2020-12-19 15:29:13 -0800

    fun

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var name = prompt("What is your name?");
-alert("Hi " + name)
-
-confirm("Please press a button");
+var user = prompt("What is your name?");
+if(user !== ""){
+    alert("Hi " + user)
+    confirm("Please press a button")
+}else{
+    alert("Please enter your name");
+    user = prompt("What is your name?");
+    alert("Hi " + user)
+    confirm("Please press a button")
+}
